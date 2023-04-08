b3b5c89189336d8fa843ae821a5693af200c483b
YURII FEDOROV <46714655+DALAKEN@users.noreply.github.com>
2023-04-08 16:09:21 +0300

    Create Steam Guard Code

---
+++ Steam-Guard-Code/Program.cs
@@ -0,0 +1,26 @@
+﻿using SteamAuth;
+
+static class SteamTOTP
+{
+	public static string GenerateSteamCode(string sharedSecret)
+	{
+		SteamGuardAccount account = new SteamGuardAccount();
+		account.SharedSecret = sharedSecret;
+		return account.GenerateSteamGuardCode();
+	}
+}
+
+class Program
+{
+	public static void Main()
+	{
+		string sharedSecret = "3Ov0jBh9L0EMlP4fLu0tvtFIt9g=";
+
+		while (true)
+		{
+			Console.Clear();
+			Console.WriteLine($"Steam Guard: {SteamTOTP.GenerateSteamCode(sharedSecret)}");
+			System.Threading.Thread.Sleep(1000);
+		}
+	}
+}
